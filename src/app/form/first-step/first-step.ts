a429bd1205d56a63358d9038b6b2fc5e79389aa6
Morfeo <gastongomez2014@hotmail.com>
2026-01-26 23:11:27 -0300

    componentes basicos agregados

---
+++ src/app/form/first-step/first-step.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-step',
+  imports: [],
+  templateUrl: './first-step.html',
+  styleUrl: './first-step.css',
+})
+export class FirstStep {
+
+}
---
+++ src/app/form/fourth-step/fourth-step.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fourth-step',
+  imports: [],
+  templateUrl: './fourth-step.html',
+  styleUrl: './fourth-step.css',
+})
+export class FourthStep {
+
+}
